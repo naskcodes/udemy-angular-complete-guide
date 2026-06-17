addd5b6df1e3b8234f34f9b4cf2ec2257f79dcf6
Roberta Nascimento <naskcodes@gmail.com>
2026-06-17 15:25:19 -0300

    segurança

--- 01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,18 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
   get imagePath() {
     return 'assets/users/users/' + this.avatar;
   }
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/users/' + this.avatar();
+  // });
   
   onSelectUser() {
     
